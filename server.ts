e78b5a8f1be1f24dde77f8157eca34136e865a72
SEOKHWALEE <smmc05004@hanmail.net>
2021-01-09 11:06:03 +0900

    add server add server ts config

---
+++ server.ts
@@ -0,0 +1,43 @@
+import next from "next";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const dev: boolean = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const port: number = 8080;
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+    server.use(morgan("dev"));
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: true }));
+    server.use(cookieParser(process.env.COOKIE_SECRET));
+    server.use(
+      session({
+        secret: `${process.env.SESSION_SECRET}`,
+        resave: false,
+        saveUninitialized: true,
+      })
+    );
+
+    server.all("*", (req: express.Request, res: express.Response) => {
+      return handle(req, res);
+    });
+
+    server.listen(port, () => {
+      console.log(`Listening on ${port}`);
+    });
+  })
+  .catch((ex) => {
+    console.error(ex);
+    process.exit(1);
+  });
