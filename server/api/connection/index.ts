520a6abec5e1b232d784e85240c0e6edd7ae3094
이 석화 <shlee@domain.aispera.com>
2021-02-18 16:41:02 +0900

    mod db커넥션 변수 문자열 type casting 제거

--- server/api/connection/index.ts
+++ server/api/connection/index.ts
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 const host = process.env.NEXT_PUBLIC_HOST;
-const user = String(process.env.NEXT_PUBLIC_USER);
-const password = String(process.env.NEXT_PUBLIC_PASSWORD);
+const user = process.env.NEXT_PUBLIC_USER;
+const password = process.env.NEXT_PUBLIC_PASSWORD;
 const DB = process.env.NEXT_PUBLIC_DB;
 const Port = Number(process.env.NEXT_PUBLIC_PORT);
 
