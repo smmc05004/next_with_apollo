b19e780bf3bb16de4b9b5d58255307d6cb9d2305
이 석화 <shlee@domain.aispera.com>
2021-03-05 14:25:08 +0900

    add 대한민국 지도 표시

--- pages/trip/index.tsx
+++ pages/trip/index.tsx
@@ -3,80 +3,79 @@ import * as GeoJSON from "d3";
 import * as d3 from "d3";
 // import MAP_GEOSON from "../../components/common/korea.json";
 import MAP_GEOSON from "../../components/common/map.geo.json";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { feature } from "topojson-client";
+import { Feature, FeatureCollection, Geometry } from "geojson";
+import { geoEqualEarth, geoPath } from "d3-geo";
 
-const Container = styled.div`
-  width: 700px;
-  min-height: 700px;
-  float: left;
-  margin: 15px 35px;
-`;
-const States = styled.div``;
-
-const path = styled.div`
-  fill: #585858;
-  stroke: #000000;
-  stroke-width: 1.5px;
+const Path = styled.path<{ myCode: number; activeCode: number }>`
+  stroke: ${(props) =>
+    props.myCode === props.activeCode ? "red" : "transparent"};
+  cursor: pointer;
   &:hover {
-    fill: #009300;
+    stroke: red;
   }
 `;
 
 const Trip = () => {
-  const drawMap = (target: string) => {
-    const mapjson = MAP_GEOSON;
+  const width = 700; //지도의 넓이
+  const height = 700; //지도의 높이
+  const initialScale = 5500; //확대시킬 값
+  const initialX = -11900; //초기 위치값 X
+  const initialY = 4050; //초기 위치값 Y
+  let labels;
 
-    const width = 700; //지도의 넓이
-    const height = 700; //지도의 높이
-    const initialScale = 5500; //확대시킬 값
-    const initialX = -11900; //초기 위치값 X
-    const initialY = 4050; //초기 위치값 Y
-    let labels;
+  const [active, setActive] = useState<number>(0);
 
-    // 지도 출력 방법 선택
-    const projection = d3
-      .geoMercator()
-      .scale(initialScale)
-      .translate([initialX, initialY]);
-    console.log("projection: ", projection);
+  const projection = d3
+    .geoMercator()
+    .scale(initialScale)
+    .translate([initialX, initialY]);
 
-    const path = d3.geoPath().projection(projection);
-    console.log("path: ", path);
-
-    // 지도를 그릴 svg
-    const svg = d3
-      .select(target)
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("id", "map")
-      .attr("class", "map");
-
-    // 지도가 그려지는 그래픽 노드(g) 생성
-    const states = svg.append("g").attr("id", "states");
-
-    states
-      .append("rect")
-      .attr("class", "background")
-      .attr("width", width)
-      .attr("height", height);
-
-    states
-      .selectAll("path")
-      .data(MAP_GEOSON.features)
-      .enter()
-      .append("path")
-      .attr("d", path);
+  const onClick = (e: React.MouseEvent<HTMLOrSVGElement>, id: number) => {
+    // console.log(e.target);
+    console.log("id", id);
+    console.log("클릭");
+    setActive(id);
   };
 
-  useEffect(() => {
-    drawMap("#map-wrapper");
-  }, []);
+  // const onwheel = () => {
+  //   const zoomed = () => {
+  //     console.log("zoom active");
+  //   };
+
+  //   const paths = d3.selectAll("path");
+  //   paths.call(d3.zoom().on("zoom", zoomed));
+  // };
 
   return (
     <div>
       <span>trip 페이지</span>
-      <div id="map-wrapper"></div>
+      <div id="map-wrapper">
+        <svg width={700} height={700}>
+          <g>
+            {(MAP_GEOSON as FeatureCollection).features.map((d, i) => {
+              let id = 0;
+
+              if (d.properties) {
+                id = d.properties.SGG_Code;
+              }
+
+              return (
+                <Path
+                  myCode={id}
+                  activeCode={active}
+                  key={i}
+                  d={geoPath().projection(projection)(d) as string}
+                  onClick={(e) => onClick(e, id)}
+                  // onWheel={onwheel}
+                  stroke="white"
+                />
+              );
+            })}
+          </g>
+        </svg>
+      </div>
     </div>
   );
 };
